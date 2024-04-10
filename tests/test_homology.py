7b544c48e70524033922fec4bbcfc7fd22b218ca
Bastian Rieck <bastian@rieck.me>
2024-04-10 17:14:06 +0200

    Basic check for proper Betti numbers

---
+++ tests/test_homology.py
@@ -0,0 +1,56 @@
+"""Test correctness of homology information in an input file."""
+
+import gudhi as gd
+
+import argparse
+import itertools
+import operator
+import json
+
+
+def build_simplex_tree(top_level_simplices):
+    simplices = set([tuple(s) for s in top_level_simplices])
+    max_dim = len(next(iter(simplices)))
+
+    for simplex in top_level_simplices:
+        for dim in range(1, max_dim):
+            simplices.update(s for s in itertools.combinations(simplex, r=dim))
+
+    # Get every complex into lexicographic order. This
+    # requires converting everything back to a list.
+    simplices = list(simplices)
+    simplices.sort()
+    simplices.sort(key=len)
+
+    st = gd.SimplexTree()
+    for simplex in simplices:
+        st.insert(simplex)
+
+    return st
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("INPUT", type=str, help="Input file")
+
+    args = parser.parse_args()
+
+    with open(args.INPUT) as f:
+        data = json.load(f)
+
+    for data_ in data:
+        simplex_tree = build_simplex_tree(data_["triangulation"])
+        persistence_pairs = simplex_tree.persistence(persistence_dim_max=True)
+
+        dimensions = range(data_["dimension"] + 1)
+
+        betti_numbers = []
+
+        for dimension in dimensions:
+            pairs = [
+                (a, b) for dim, (a, b) in persistence_pairs if dim == dimension
+            ]
+
+            betti_numbers.append(len(pairs))
+
+        assert data_["betti_numbers"] == betti_numbers
