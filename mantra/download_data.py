f9647f306ab06f6e7b8cef3ecc67717e39d43155
ErnstRoell <ernstroell@gmail.com>
2024-04-11 20:30:47 +0200

    Added download script for fetching data

---
+++ mantra/download_data.py
@@ -0,0 +1,14 @@
+import requests
+
+def download_data():
+    response = requests.get("https://www3.math.tu-berlin.de/IfM/Nachrufe/Frank_Lutz/stellar/2_manifolds_all.txt")
+    with open("./raw_data/2_manifolds_all.txt","w") as f: 
+        f.write(response.text)
+
+    response = requests.get("https://www3.math.tu-berlin.de/IfM/Nachrufe/Frank_Lutz/stellar/2_manifolds_all_type.txt")
+    with open("./raw_data/2_manifolds_type.txt","w") as f: 
+        f.write(response.text)
+
+    response = requests.get("https://www3.math.tu-berlin.de/IfM/Nachrufe/Frank_Lutz/stellar/2_manifolds_all_hom.txt")
+    with open("./raw_data/2_manifolds_hom.txt","w") as f: 
+        f.write(response.text)
