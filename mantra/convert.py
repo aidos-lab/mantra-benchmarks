aaa73a8203c050c7132ed321ff129dab039aa662
ErnstRoell <ernstroell@gmail.com>
2024-04-12 13:19:38 +0200

    Reworked convert.py. Decoupled, ready for testing.

--- mantra/convert.py
+++ mantra/convert.py
@@ -1,252 +1,118 @@
-"""Convert from lexicographical format to line-based format.
-
-The purpose of this script is to convert triangulations from
-a lexicographical format to a line-based format. In the old,
-i.e. lexicographical, format, triangulations can span *more*
-than one line, and every line contains a different number of
-vertices. This makes parsing the format cumbersome.
-
-As an alternative, this script parses the triangulations and
-represents them as JSON objects. This facilitates *storing*,
-as well as *processing* the data types.
-"""
-
 import argparse
 import json
 import re
 
-import numpy as np
 
-
-def process_triangulation(triangulation):
-    """Process an individual triangulation.
-
-    A triangulation is represented (following the original data format)
-    as a newline-separated string of vertex indices. These indices will
-    be parsed into a (nested) array of integers and returned in a dict,
-    which contains additional information about the triangulation.
-
-    Returns
-    -------
-    dict
-        A dictionary containing information about the triangulation. The
-        keys of dict are "triangulation", "dimension", and "n_vertices".
-        The triangulation itself is stored as a nested list of vertices,
-        representing the top-level simplices of the triangulation.
+def process_triangulation_line(line):
+    """'
+    Parses a single line of the triangluation and returns the fields in a
+    dictionary. The input string has to have the format
+    'manifold_{dim}_{n_vert}_{non-unique-id}=[[1,2,3],...,[1,4,3]]'
+    Returns dictionary with triangulation as list of lists, dimension and number
+    of vertices.
     """
-    simplices = np.asarray(
-        [
-            np.fromstring(line, sep=",", dtype=int)
-            for line in triangulation.split("\n")
-        ]
-    )
+    line = re.split("=", line)
 
-    dimensions = [len(simplex) - 1 for simplex in simplices]
-    assert dimensions[1:] == dimensions[:-1]
+    # Parse dimension and n_vertices from id.
+    match = re.match("manifold_(\d+)_(\d+)", line[0])
 
-    vertices = set(simplices.flatten())
-
+    # json.loads loads string into List[List[int]].
     return {
-        "triangulation": simplices.tolist(),
-        "dimension": dimensions[0],
-        "n_vertices": len(vertices),
+        line[0]: {
+            "id": line[0],
+            "dimension": match.group(1),
+            "n_vertices": match.group(2),
+            "triangulation": json.loads(line[1]),
+        }
     }
 
 
-def parse_topological_type(s):
-    """Parse the type field of a manifold.
+def process_triangulation(content: str):
+    lines = content.removesuffix("\n\n")
+    lines = re.split("\n\n", lines)
+    lines = [re.sub("\n\s+", "", line) for line in lines]
 
-    The type field of an entry consists of a flag indicating whether the
-    manifold is orientable, followed by its genus. Optionally, there may
-    be a canonical name, such as "Klein bottle."
+    dicts = [process_triangulation_line(line) for line in lines]
+    return {k: v for d in dicts for k, v in d.items()}
 
-    Parameters
-    ----------
-    s: string
-        Input string containing topological type information.
 
-    Returns
-    -------
-    dict
-        A dictionary containing information about the topological type,
-        using the keys "name" (for an optional canonical name, which is
-        allowed to be the empty string), "orientable" (a boolean flag),
-        and "genus" (an integer).
+def process_type_line(line: str) -> dict:
     """
-    parts = s.split("=")
-    assert len(parts) == 1 or len(parts) == 2
+    Parses a single line with the homology type. The line has to have the format
+    'manifold_{dim}_{n_vert}_{non-unique-id}:  ( {orientable} ; {genus} ) =
+    {name}'. The name field is optional and may be omitted. The function returns
+    a dictionary with all fields parsed.
 
-    result = {"name": "", "orientable": None}
+    Examples:
+    - 'manifold_2_9_645:  ( - ; 1 ) = RP^2'
+    - 'manifold_2_9_646:  ( - ; 2 ) = Klein bottle'
+    - 'manifold_2_9_587:  ( - ; 3 )'
 
-    if len(parts) == 2:
-        result["name"] = parts[1].strip()
-
-    # Parse the "topological type" field, consisting of a bracketed
-    # expression indicating whether the manifold is orientable, and
-    # the genus.
-    topological_type = parts[0]
-    topological_type = topological_type.replace("(", "")
-    topological_type = topological_type.replace(")", "")
-
-    orientable, genus = topological_type.split(";")
-    orientable = orientable.strip()
-
-    result["genus"] = int(genus)
-
-    assert orientable in ["+", "-"]
-
-    if orientable == "+":
-        result["orientable"] = True
-    else:
-        result["orientable"] = False
-
-    return result
-
-
-def parse_homology_groups(s):
-    """Parse information about the homology groups of a manifold.
-
-    Homology group information is represented as a tuple of ranks, with
-    each entry either a number or a number with torsion information. An
-    example would be "(1, 0 + Z_2, 0)".
-
-    This information will be represented in two tuples:
-
-    1. A "betti_numbers" list, consisting of a list of ranks of the
-       homology groups, ignoring any torsion.
-    2. A "torsion_coefficients" list, providing torsion coefficient
-       information. Each torsion coefficient is stored as-is, i.e.,
-       as a string. An empty torsion coefficient is indicated using
-       an empty string.
-
-    Parameters
-    ----------
-    s : string
-        String containing information about the homology groups of the
-        triangulation.
-
-    Returns
-    -------
-    dict
-        A dictionary containing the homology information of the
-        triangulation (see above).
+    Input:
+        String
+    Output:
+        Dict
     """
-    s = s.replace("(", "")
-    s = s.replace(")", "")
-
-    ranks = s.split(",")
-
-    result = {
-        "betti_numbers": [],
-        "torsion_coefficients": [],
+    match = re.match(
+        r"(manifold_.*):\s+\( ([+-])\s;\s(\d)\s\)(\s=\s)?(.*)?", line
+    )
+    return {
+        match.group(1): {
+            "id": match.group(1),
+            "orientable": True if match.group(2) == "+" else False,
+            "genus": match.group(3),
+            "name": match.group(5),
+        }
     }
 
-    for rank in ranks:
-        rank = rank.split("+")
-        assert len(rank) == 1 or len(rank) == 2
 
-        result["betti_numbers"].append(int(rank[0]))
-
-        # Single entry only, so no torsion to consider.
-        if len(rank) == 1:
-            result["torsion_coefficients"].append("")
-
-        # More than one entry. Store the torsion coefficient as-is.
-        else:
-            result["torsion_coefficients"].append(rank[1].strip())
-
-    return result
+def process_type(content):
+    lines = content.removesuffix("\n").split("\n")
+    dicts = [process_type_line(line) for line in lines]
+    return {k: v for d in dicts for k, v in d.items()}
 
 
-def process_homology_groups_or_types(filename, parse_fn):
-    """Process information about homology groups or topological types.
-
-    The data format for the homology groups or the topological type of
-    a triangulation is easier to parse than the triangulation since it
-    only uses a single string, which provides the Betti numbers (and a
-    summand for torsion), or a description of the type of the manifold
-    that is triangulated.
-
-    Parameters
-    ----------
-    parse_fn : callable
-        Function to use for further processing each entry.
-
-    Returns
-    -------
-    dict
-        Dictionary, with keys indicating the respective triangulation
-        and values being strings corresponding to homology groups, or
-        type information, respectively. No further processing of each
-        string is attempted.
-    """
-    with open(filename) as f:
-        lines = f.readlines()
-
-    matches = [re.match(r"(manifold_.*):\s+(.*)$", line) for line in lines]
-
-    return {match.group(1): parse_fn(match.group(2)) for match in matches}
+def process_homology_line(line):
+    match = re.match(r"(manifold_.*):\s+\((.*)\)", line)
+    tc, bn = [], []
+    for rank in match.group(2).split(", "):
+        rank_match = re.match("(\d+)(\s\+\s)?(.*)?", rank)
+        bn.append(int(rank_match.group(1)))
+        tc.append(rank_match.group(3))
+    return {match.group(1): {"torsion_coefficients": tc, "betti_numbers": bn}}
 
 
-def process_triangulations(filename):
-    """Process file in lexicographical format."""
-    with open(filename) as f:
-        lines = f.read()
-        lines = lines.split("\n\n")
-
-    # Get everything on a single line first and remove all empty lines
-    # or blocks from the resulting array.
-    lines = [line.replace("\n", "") for line in lines]
-    lines = [line for line in lines if line]
-
-    names = [re.match(r"(manifold_.*)=", line).group(1) for line in lines]
-
-    lines = [re.sub(r"manifold_(.*)=", "", line) for line in lines]
-    lines = [re.sub(r"\s+", "", line) for line in lines]
-    lines = [re.sub(r"],", "]\n", line) for line in lines]
-    lines = [re.sub(r"\]\]", "]", line) for line in lines]
-    lines = [re.sub(r"\[\[", "[", line) for line in lines]
-    lines = [re.sub(r"[\[\]]", "", line) for line in lines]
-
-    assert len(lines) == len(names)
-
-    triangulations = lines
-    triangulations = {
-        name: process_triangulation(triangulation)
-        for name, triangulation in zip(names, triangulations)
-    }
-
-    return triangulations
+def process_homology(lines):
+    lines = lines.removesuffix("\n").split("\n")
+    dicts = [process_homology_line(line) for line in lines]
+    return {k: v for d in dicts for k, v in d.items()}
 
 
 def process_manifolds(
     filename_triangulation, filename_homology=None, filename_type=None
 ):
-    triangulations = process_triangulations(filename_triangulation)
+    # Parse triangulations
+    with open(filename_triangulation) as f:
+        lines = f.read()
+    triangulations = process_triangulation(lines)
 
+    # Parse homology
     if filename_homology:
-        homology_groups = process_homology_groups_or_types(
-            filename_homology, parse_homology_groups
-        )
+        with open(filename_homology) as f:
+            lines = f.read()
+        homology_groups = process_homology(lines)
 
-        for manifold in triangulations:
-            triangulations[manifold].update(homology_groups[manifold])
-
+    # Parse type
     if filename_type:
-        types = process_homology_groups_or_types(
-            filename_type, parse_topological_type
+        with open(filename_type) as f:
+            lines = f.read()
+        types = process_type(lines)
+
+    # Merge all together
+    for manifold in triangulations:
+        triangulations[manifold].update(
+            homology_groups[manifold] | types[manifold]
         )
-
-        for manifold in triangulations:
-            triangulations[manifold].update(types[manifold])
-
-    # Turn ID into a separate attribute. This enables us to turn the
-    # whole data set into a list of triangulations, making it easier
-    # to add new triangulations later on.
-    triangulations = [
-        {"id": manifold, **triangulations[manifold]}
-        for manifold in triangulations
-    ]
 
     return triangulations
 
