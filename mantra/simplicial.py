4da55f4d7a3e35871dd10d5dac9545e1bbc2e4f2
Rubén Ballester <rballeba@gmail.com>
2024-04-24 18:44:43 +0200

    Adding versioning and cleaning a little bit the download part.

--- mantra/simplicial.py
+++ mantra/simplicial.py
@@ -9,15 +9,28 @@ from mantra.convert import process_manifolds
 
 
 class SimplicialDataset(InMemoryDataset):
+    available_versions = ['1.0.0']
+
+    @staticmethod
+    def _get_raw_dataset_root_link(version: str):
+        match version:
+            case "1.0.0":
+                return "https://www3.math.tu-berlin.de/IfM/Nachrufe/Frank_Lutz/stellar"
+            case _:
+                raise ValueError(f"Version {version} not available")
+
     def __init__(
-        self,
-        root,
-        transform=None,
-        pre_transform=None,
-        pre_filter=None,
+            self,
+            root,
+            transform=None,
+            pre_transform=None,
+            pre_filter=None,
+            version='1.0.0'
     ):
+        assert version in self.available_versions
+        self.version = version
         self.manifold = "2"
-        root += "/simplicial"
+        root += f"/simplicial_v{version}"
         super().__init__(root, transform, pre_transform, pre_filter)
         self.load(self.processed_paths[0])
 
@@ -34,20 +47,12 @@ class SimplicialDataset(InMemoryDataset):
         return ["data.pt"]
 
     def download(self):
-        download_url(
-            f"https://www3.math.tu-berlin.de/IfM/Nachrufe/Frank_Lutz/stellar/{self.manifold}_manifolds_all.txt",
-            self.root + "/raw",
-        )
-
-        download_url(
-            f"https://www3.math.tu-berlin.de/IfM/Nachrufe/Frank_Lutz/stellar/{self.manifold}_manifolds_all_type.txt",
-            self.root + "/raw",
-        )
-
-        download_url(
-            f"https://www3.math.tu-berlin.de/IfM/Nachrufe/Frank_Lutz/stellar/{self.manifold}_manifolds_all_hom.txt",
-            self.root + "/raw",
-        )
+        root_link = self._get_raw_dataset_root_link(self.version)
+        for name in self.raw_file_names:
+            download_url(
+                f"{root_link}/{name}",
+                self.raw_dir,
+            )
 
     def process(self):
         triangulations = process_manifolds(
