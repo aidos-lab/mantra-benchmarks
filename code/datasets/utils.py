50023a30c76e2ef3d5e1d90e3a6ba1c67e4f3b54
Rubén Ballester <rballeba@gmail.com>
2024-11-21 16:23:06 +0100

    Solving bug for 3D feature generation.

--- code/datasets/utils.py
+++ code/datasets/utils.py
@@ -162,7 +162,7 @@ def append_signals(data, signals_key, signals):
     if signals_key not in data.x:
         data.x[signals_key] = signals
     else:
-        data.x[signals_key] = torch.concatenate([data.x[1], signals], dim=1)
+        data.x[signals_key] = torch.concatenate([data.x[signals_key], signals], dim=1)
     return data
 
 
