14555a4f5509ad7c60852f4af64f9134c67e220f
binbash <danielbinschmid@outlook.de>
2025-01-25 06:07:42 +0100

    reverted change in generate_configs

--- code/generate_configs.sh
+++ code/generate_configs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo /deps/venv/bin/python ./experiments/generate_configs.py \
-    --max_epochs 20 \
+python ./experiments/generate_configs.py \
+    --max_epochs 6 \
     --lr 0.001 \
     --config_dir "/data/configs" \
